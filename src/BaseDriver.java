6ac59e77c736ca398c54ca8f8b2872675e8f13a4
mustafayumrutepe <mstfyumrutepe@gmail.com>
2024-06-27 16:08:22 +0300

    somethings fixed

--- src/BaseDriver.java
+++ src/BaseDriver.java
@@ -1,11 +1,9 @@
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
-
 import java.time.Duration;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-
 public class BaseDriver {
 
 public static WebDriver driver;
