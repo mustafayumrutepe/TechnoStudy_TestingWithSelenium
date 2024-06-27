1e1aac9f1ac77805c230602d8f58a72d62ed15b6
mustafayumrutepe <mstfyumrutepe@gmail.com>
2024-06-27 10:27:57 +0300

    ClickToLogo testing started

--- src/Tests.java
+++ src/Tests.java
@@ -4,6 +4,7 @@ import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.Select;
 
 import java.util.Iterator;
 import java.util.List;
@@ -11,6 +12,7 @@ import java.util.Set;
 
 public class Tests extends BaseDriver{
     Actions actions=new Actions(driver);
+
     @Test()
     public void TermsOfUse(){
         WebElement termsOfUse= driver.findElement(By.linkText("Kullanım Şartlarını"));
@@ -43,6 +45,26 @@ public class Tests extends BaseDriver{
 
             driver.navigate().back();
         }
+        WaitAndQuit();
+    }
+
+    @Test
+    public void ClickToLogo(){
+        WebElement coursesBtn=driver.findElement(By.xpath("//a[text()='Kurslar']"));
+        actions.moveToElement(coursesBtn).build().perform();
+        List<WebElement> menu=driver.findElements(By.xpath("//a[@role='menuitem']"));
+
+        for (WebElement e : menu){
+            e.click();
+            OptionalWait(2);
+            WebElement logo= driver.findElement(By.cssSelector("[style='max-height: 80px']"));
+            Assert.assertTrue("The logo is not visible on the site.",logo.isDisplayed());
+
+            logo.click();
+            OptionalWait(2);
+            Assert.assertTrue("Home page have not opened",driver.getCurrentUrl().contains("https://techno.study/tr"));
+        }
+
         WaitAndQuit();
     }
 }
